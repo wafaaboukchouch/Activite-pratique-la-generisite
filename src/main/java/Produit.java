939111bc320f3b4f62cde277f2f1adaad5c57b14
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-02 22:51:00 +0200

    Update Produit.java

--- src/main/java/Produit.java
+++ src/main/java/Produit.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
-
 public class Produit {
+    // Les caractéristiques d'un produit (attributs)
+    private long id;
+    private String nom;
+    private String marque;
+    private double prix;
+    private String description;
+    private int nombreEnStock;
+    
+    // Constructeur : permet de créer un produit en donnant toutes ses infos
+    public Produit(long id, String nom, String marque, double prix, 
+                   String description, int nombreEnStock) {
+        this.id = id;
+        this.nom = nom;
+        this.marque = marque;
+        this.prix = prix;
+        this.description = description;
+        this.nombreEnStock = nombreEnStock;
+    }
+    
+    // Getters : permettent de récupérer les infos d'un produit
+    public long getId() {
+        return id;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+    
+    public String getMarque() {
+        return marque;
+    }
+    
+    public double getPrix() {
+        return prix;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getNombreEnStock() {
+        return nombreEnStock;
+    }
+    
+    // Setters : permettent de modifier les infos d'un produit
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+    
+    public void setNombreEnStock(int nombreEnStock) {
+        this.nombreEnStock = nombreEnStock;
+    }
+    
+    // Cette méthode permet d'afficher joliment un produit
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Nom: " + nom + " | Marque: " + marque + 
+               " | Prix: " + prix + " DH | Stock: " + nombreEnStock;
+    }
 }
