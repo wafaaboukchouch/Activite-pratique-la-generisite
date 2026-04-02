4052678e4b2e3f9095c22e0a6fe5d847305e053a
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-02 22:51:42 +0200

    Update IMetier.java

--- src/main/java/IMetier.java
+++ src/main/java/IMetier.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.List;
 
-public interface IMetier {
+// T = Type générique (on décidera plus tard quel type utiliser)
+public interface IMetier<T> {
+    
+    // Ajouter un objet à la liste
+    public void add(T o);
+    
+    // Récupérer TOUS les objets sous forme de liste
+    public List<T> getAll();
+    
+    // Trouver un objet par son ID
+    public T findById(long id);
+    
+    // Supprimer un objet par son ID
+    public void delete(long id);
 }
